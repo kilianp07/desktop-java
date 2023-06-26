9919260776381c421e2b8b48e921124e51512b0c
kilianp07 <kilianp07@icloud.com>
2023-06-26 12:04:09 +0200

    added activity model

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,2 +1,51 @@
-package org.example.model;public class User {
+package org.example.model;
+
+import java.util.Date;
+
+public class User {
+    private String name;
+    private String surname;
+    private Date birthdate;
+    private String sex;
+
+    public User(String name, String surname, Date birthdate, String sex) {
+        this.name = name;
+        this.surname = surname;
+        this.birthdate = birthdate;
+        this.sex = sex;
+    }
+
+    // Getters and setters for the properties
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
