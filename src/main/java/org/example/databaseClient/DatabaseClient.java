bcbe64940e27ba23f4b177e576bd0916bf2ab30a
kilianp07 <57315153+kilianp07@users.noreply.github.com>
2023-06-27 16:04:23 +0200

    Calculate trainings (#25)
    * Add getBewteenTwoDates method
    
    * Add getBewteenTwoDates method

--- src/main/java/org/example/databaseClient/DatabaseClient.java
+++ src/main/java/org/example/databaseClient/DatabaseClient.java
@@ -1,39 +1,45 @@
 package org.example.databaseClient;
 
 import com.mongodb.*;
-import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.result.InsertOneResult;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
 import org.example.model.Activity.Activity;
 import org.example.model.User.User;
+import org.example.repository.ActivityManager;
+import org.example.repository.IActivityManager;
 import org.example.repository.IUserManager;
 import org.example.repository.UserManager;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.example.mapper.ActivityMapper.ActivityToDocument;
 import static org.example.mapper.UserMapper.UserToDocument;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.ArrayList;
-import java.util.Date;
-
 public class DatabaseClient {
     private MongoCollection<Document> userCollection;
-    private MongoCollection<Document> activityCollection;
+
     private MongoClient mongoClient;
     private static String connectionString = "";
     private MongoDatabase database;
     private IUserManager userManager;
+
+    private IActivityManager activityManager;
+
     public DatabaseClient() {
         Dotenv dotenv = Dotenv.configure().load();
         connectionString = dotenv.get("ConnectionString");
         this.mongoClient = MongoClients.create(connectionString);
         this.database = mongoClient.getDatabase("DESKTOP_YNOV_DATABASE");
         this.userCollection = database.getCollection("user");
-        this.activityCollection = database.getCollection("activity");
         userManager = new UserManager(userCollection);
+        this.activityManager = new ActivityManager(userCollection);
 
     }
     public void init() {
@@ -81,22 +87,11 @@ public class DatabaseClient {
         }
     }
 
-    public MongoClient getMongoClient() {
-        return mongoClient;
+    public List<Activity> getActivitiesBetweenDates(String userID, Date startDate, Date endDate) {
+        return activityManager.getBetweenDates(userID, startDate, endDate);
     }
 
-    public ArrayList<User> getUsers(){
-        FindIterable<Document> documents = userCollection.find();
-        ArrayList<User> users = new ArrayList<>();
-        for (Document document : documents) {
-            String username = document.getString("name");
-            String surname = document.getString("surname");
-            Date birthdate = document.getDate("birthdate");
-            String sex = document.getString("sex");
-            ArrayList<Activity> activityList = (ArrayList<Activity>) document.getList("activityList", Activity.class);
-            User user = new User(username, surname, birthdate, sex, activityList);
-            users.add(user);
-        }
-        return users;
+    public MongoClient getMongoClient() {
+        return mongoClient;
     }
 }
