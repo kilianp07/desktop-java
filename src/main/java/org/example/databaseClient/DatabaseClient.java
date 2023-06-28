7928cfbf6381415c10ad7f455605181df65bc9e9
Seddik Mehdi <mhdi.seddik@gmail.com>
2023-06-28 09:11:42 +0200

    fix: fix new activity

--- src/main/java/org/example/databaseClient/DatabaseClient.java
+++ src/main/java/org/example/databaseClient/DatabaseClient.java
@@ -1,14 +1,10 @@
 package org.example.databaseClient;
 
 import com.mongodb.*;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
-import org.example.platform.IUserPlatform;
-import org.example.platform.UserPlatform;
+
 
 public class DatabaseClient {
     private MongoCollection<Document> userCollection;
@@ -16,19 +12,14 @@ public class DatabaseClient {
     private MongoClient mongoClient;
     private static String connectionString = "";
     private MongoDatabase database;
-    private IUserPlatform userPlatform;
 
 
     public DatabaseClient() {
-
         Dotenv dotenv = Dotenv.configure().load();
         connectionString = dotenv.get("ConnectionString");
         this.mongoClient = MongoClients.create(connectionString);
         this.database = mongoClient.getDatabase("DESKTOP_YNOV_DATABASE");
-        this.setUserCollection(database.getCollection("user"));
-        userPlatform = new UserPlatform(userCollection);
-
-
+        this.userCollection = database.getCollection("user");
     }
     public void init() {
         ServerApi serverApi = ServerApi.builder()
@@ -45,15 +36,6 @@ public class DatabaseClient {
         } catch (MongoException e) {
             e.printStackTrace();
         }
-    }
-
-
-    public MongoCollection<Document> getUserCollection() {
-        return userCollection;
-    }
-
-    public void setUserCollection(MongoCollection<Document> userCollection) {
-        this.userCollection = userCollection;
     }
 
     public MongoClient getMongoClient() {
