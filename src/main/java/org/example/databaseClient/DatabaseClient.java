9e672d3f7c5211c64d7c1d17e12d67d57d0492fb
Seddik Mehdi <mhdi.seddik@gmail.com>
2023-06-28 09:11:42 +0200

    fix: fix new activity

--- src/main/java/org/example/databaseClient/DatabaseClient.java
+++ src/main/java/org/example/databaseClient/DatabaseClient.java
@@ -1,10 +1,7 @@
 package org.example.databaseClient;
 
 import com.mongodb.*;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import com.mongodb.client.result.InsertOneResult;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.bson.Document;
